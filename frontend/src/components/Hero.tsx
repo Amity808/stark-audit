4723031f2095501c674825d5acb060d4198fa3da
amity808 <bolarinwamuhdsodiq0@gmail.com>
2024-10-28 09:29:45 +0100

    update UI implementation

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { motion } from "framer-motion";
+import React from "react";
+import { AuroraBackground } from "./ui/aurora-background";
+
+export function Hero() {
+  return (
+    <AuroraBackground>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="relative flex flex-col gap-4 items-center justify-center px-4"
+      >
+        <div className="text-3xl md:text-7xl font-bold text-purple-500 dark:text-purple-500 text-center">
+          StarkAudit is a decentralized platform to audit your starknet smart contract using AI
+        </div>
+        <div className="font-extralight text-base md:text-4xl text-purple-500 dark:text-purple-500 py-4">
+          Audit your smart contract to improve your smart contract performance
+        </div>
+        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+            Audit Today
+        </button>
+      </motion.div>
+    </AuroraBackground>
+  );
+}
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,4 +1,7 @@
 import type { Config } from "tailwindcss";
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config: Config = {
   content: [
@@ -12,8 +15,33 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        aurora: "aurora 60s linear infinite",
+      },
+      keyframes: {
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [require('daisyui'),],
 };
 export default config;
+
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
