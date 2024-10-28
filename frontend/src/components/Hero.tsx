e6cd4b27478c39d4c6da774eef87848c1c0cc08a
amity808 <bolarinwamuhdsodiq0@gmail.com>
2024-10-28 10:09:39 +0100

    update UI

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -18,13 +18,13 @@ export function Hero() {
         className="relative flex flex-col gap-4 items-center justify-center px-4"
       >
         <div className="text-3xl md:text-7xl font-bold text-purple-500 dark:text-purple-500 text-center">
-          StarkAudit is a decentralized platform to audit your starknet smart contract using AI
+          StarkAudit A Decentralized Platform To Audit Starknet Smart Contract Using AI
         </div>
         <div className="font-extralight text-base md:text-4xl text-white dark:text-white py-4">
           Audit your smart contract to improve your smart contract performance
         </div>
         <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
-            Audit Today
+            <a href="/audit">Audit</a>
         </button>
       </motion.div>
     </AuroraBackground>
---
+++ frontend/src/pages/audit.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import StartAudit from "../components/StarkAudit"
+const Aduit = () => {
+  return (
+    <div>
+        <div>
+            <StartAudit />
+        </div>
+    </div>
+  )
+}
+
+export default Aduit
