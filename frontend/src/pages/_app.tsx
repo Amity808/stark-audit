2655d1cf497be1e1ab0c15486a19cd436c0fefe0
amity808 <bolarinwamuhdsodiq0@gmail.com>
2024-11-11 05:59:13 +0100

    UPDATE WALET CONNECTION

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../style/globals.css"
 import "@rainbow-me/rainbowkit/styles.css"
 import type { AppProps } from "next/app";
+import { StarknetProvider } from "@/provider/starknet-provider";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
       
         <>
+        <StarknetProvider> 
         <Component {...pageProps} />
+        </StarknetProvider>
+        
         </>
         
          
